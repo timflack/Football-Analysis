9b5a4f0cc6c6ecd7cdc13f43f038e507f2bf635f
timflack <tdeflack@gmail.com>
2020-10-25 11:59:23 +0000

    changed passing analysis to look at distances as well

--- pass_analysis.py
+++ pass_analysis.py
@@ -1,15 +1,19 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from eventsTodataframe import load_file, to_dataframe
+from plotting_pitches import plot_pitch
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
 def get_pass_df(match_df):
     """
-
+    Function that returns a dataframe of passes with an added column of distance (euclidean) of the pass
     :param match_df: output from to_dataframe function from eventsTodataframe.py
     :return pass_df: dataframe containing all passes in a match that occur in regular play:
     """
     pass_df = match_df[(match_df['type'] == 'Pass')]
+    cols1 = ['x_coord', 'y_coord']
+    cols2 = ['end_x_coord', 'end_y_coord']
+    pass_df['distance'] = np.linalg.norm(pass_df[cols1].values - pass_df[cols2].values, axis=1)
 
     return pass_df
 
@@ -25,14 +29,72 @@ def get_team_passes(pass_df, team_name):
     return df_team_passes
 
 
-def get_player_passes(pass_df, player_name):
+def get_player_passes(pass_df):
     """
 
     :param pass_df: output from get_pass_df (dataframe)
-    :param player_name: name of player you want to get passes for
     :return df_player_passes: dataframe containing open play passes for a particular player
     """
+    print(pass_df.player.unique())
+    player_name = input('Choose name from list: ')
     df_player_passes = pass_df[pass_df['player'] == player_name]
 
     return df_player_passes
 
+def plot_player_passes(player_pass_df,length, width, unity, linecolor, type='normal'):
+    """
+
+    :param player_pass_df:
+    :param length:
+    :param width:
+    :param unity:
+    :param linecolor:
+    :return:
+    """
+    # fig = plt.figure()
+    ax = plot_pitch(length=length, width=width, unity=unity, linecolor=linecolor)
+    for idx, pass_data in player_pass_df.iterrows():
+        x = pass_data['x_coord']
+        y = pass_data['y_coord']
+        dx = pass_data['end_x_coord'] - x
+        dy = pass_data['end_y_coord'] - y
+        if type == 'normal':
+            if pass_data['outcome'] == 'Complete':
+                color = 'green'
+            else:
+                color = 'red'
+            end_circle = plt.Circle((x, width - y), 1, color=color)
+            end_circle.set_alpha(.2)
+            ax.add_patch(end_circle)
+
+            pass_arrow = plt.Arrow(x, width - y, dx, -dy, width=1, color="black")
+            ax.add_patch(pass_arrow)
+        elif type == 'distance':
+            if pass_data['distance'] <= 15.0:
+                color = 'green'
+            elif 15.0 < pass_data['distance'] <= 30.0:
+                color = 'orange'
+            else:
+                color = 'red'
+            end_circle = plt.Circle((x, width - y), 1, color=color)
+            end_circle.set_alpha(.2)
+            ax.add_patch(end_circle)
+
+            pass_arrow = plt.Arrow(x, width - y, dx, -dy, width=1, color="black")
+            ax.add_patch(pass_arrow)
+        else:
+            print('You have not entered a correct type')
+            sys.exit()
+
+    return ax
+
+
+if __name__ == "__main__":
+    match_data = load_file(input('Input json file: '))
+    match_df = to_dataframe(match_data)
+    pass_df = get_pass_df(match_df)
+    player_df = get_player_passes(pass_df)
+    fig = plt.figure()
+    ax = plot_player_passes(player_df, 120, 80, 'yards', 'black')
+    ax = plot_player_passes(player_df, 120, 80, 'yards', 'black', 'distance')
+    plt.show()
--- plotting_pitches.py
+++ plotting_pitches.py
@@ -3,15 +3,17 @@ from matplotlib.patches import Arc
 import sys
 
 
-def plot_pitch(length, width, unity, linecolor):
+def plot_pitch(length, width, unity, linecolor, ax=None):
     """
-    Function inspired by FCPython
-    :param length:
-    :param width:
-    :param unity:
-    :param linecolor:
-    :return:
+    Function that plots a horizontal pitch - inspired by FCPython
+    :param length: length of pitch
+    :param width: width of pitch
+    :param unity: units of measurements length + width
+    :param linecolor: colors of lines on pitch
+    :return: ax
     """
+    if ax is None:
+        ax = plt.gca()
     # Set unity
     if unity == "meters":
         try:
@@ -21,8 +23,8 @@ def plot_pitch(length, width, unity, linecolor):
             print("You've entered the wrong pitch dimensions - try again")
             sys.exit()
 
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
+        # fig = plt.figure()
+        # ax = fig.add_subplot(1, 1, 1)
 
         # Pitch Outline & Centre Line
         plt.plot([0, 0], [0, width], color=linecolor)
@@ -85,10 +87,10 @@ def plot_pitch(length, width, unity, linecolor):
             print("You've entered the wrong pitch dimensions - try again")
             sys.exit()
         else:
-            # Create figure
-            fig = plt.figure()
-            # fig.set_size_inches(7, 5)
-            ax = fig.add_subplot(1, 1, 1)
+            # # Create figure
+            # fig = plt.figure()
+            # # fig.set_size_inches(7, 5)
+            # ax = fig.add_subplot(1, 1, 1)
 
             # Pitch Outline & Centre Line
             plt.plot([0, 0], [0, width], color=linecolor)
@@ -141,75 +143,19 @@ def plot_pitch(length, width, unity, linecolor):
     # Tidy Axes
     plt.axis('off')
 
-    return fig, ax
+    return ax
 
 
-def createPitchOld():
-    # Taken from FC Python
-    # Create figure
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-
-    # Pitch Outline & Centre Line
-    plt.plot([0, 0], [0, 90], color=linecolor)
-    plt.plot([0, 130], [90, 90], color=linecolor)
-    plt.plot([130, 130], [90, 0], color=linecolor)
-    plt.plot([130, 0], [0, 0], color=linecolor)
-    plt.plot([65, 65], [0, 90], color=linecolor)
-
-    # Left Penalty Area
-    plt.plot([16.5, 16.5], [65, 25], color=linecolor)
-    plt.plot([0, 16.5], [65, 65], color=linecolor)
-    plt.plot([16.5, 0], [25, 25], color=linecolor)
-
-    # Right Penalty Area
-    plt.plot([130, 113.5], [65, 65], color=linecolor)
-    plt.plot([113.5, 113.5], [65, 25], color=linecolor)
-    plt.plot([113.5, 130], [25, 25], color=linecolor)
-
-    # Left 6-yard Box
-    plt.plot([0, 5.5], [54, 54], color=linecolor)
-    plt.plot([5.5, 5.5], [54, 36], color=linecolor)
-    plt.plot([5.5, 0.5], [36, 36], color=linecolor)
-
-    # Right 6-yard Box
-    plt.plot([130, 124.5], [54, 54], color=linecolor)
-    plt.plot([124.5, 124.5], [54, 36], color=linecolor)
-    plt.plot([124.5, 130], [36, 36], color=linecolor)
-
-    # Prepare Circles
-    centreCircle = plt.Circle((65, 45), 9.15, color=linecolor, fill=False)
-    centreSpot = plt.Circle((65, 45), 0.8, color=linecolor)
-    leftPenSpot = plt.Circle((11, 45), 0.8, color=linecolor)
-    rightPenSpot = plt.Circle((119, 45), 0.8, color=linecolor)
-
-    # Draw Circles
-    ax.add_patch(centreCircle)
-    ax.add_patch(centreSpot)
-    ax.add_patch(leftPenSpot)
-    ax.add_patch(rightPenSpot)
-
-    # Prepare Arcs
-    leftArc = Arc((11, 45), height=18.3, width=18.3, angle=0, theta1=310, theta2=50, color=linecolor)
-    rightArc = Arc((119, 45), height=18.3, width=18.3, angle=0, theta1=130, theta2=230, color=linecolor)
-
-    # Draw Arcs
-    ax.add_patch(leftArc)
-    ax.add_patch(rightArc)
-
-    # Tidy Axes
-    plt.axis('off')
-
-    return fig, ax
-
-
-def createGoalMouth():
+def plot_goal(linecolor='black'):
+    """
+    Function that plots only goal and surrounding area - Inspired by FCPython
+    :param linecolor: colour of lines on pitch/goal
+    :return: fig, ax
+    """
     # Adopted from FC Python
     # Create figure
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-
-    linecolor = 'black'
 
     # Pitch Outline & Centre Line
     plt.plot([0, 65], [0, 0], color=linecolor)
@@ -250,5 +196,6 @@ def createGoalMouth():
 
 
 if __name__ == "__main__":
-    plot_pitch(120, 80, 'yards', 'gray')
+    plot_pitch(120, 80, 'yards', 'black')
+    plot_goal()
     plt.show()
